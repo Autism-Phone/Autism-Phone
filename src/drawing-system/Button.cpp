4b16deda57fa71e12e371502c44efcdd0e2c5b42
Pokecrafter <pokecrafterjanek123@gmail.com>
2025-04-16 23:34:17 +0200

    Życie jest piękne guziki działają

--- src/drawing-system/Button.cpp
+++ src/drawing-system/Button.cpp
@@ -1,27 +1,35 @@
 #include "Button.h"
-#include <emscripten/val.h>
-#include <string>
-#include <iostream>
 
-using namespace emscripten;
+EM_BOOL Button::handleClick(int eventType, const EmscriptenMouseEvent* e, void* userData) {
+    Button* self = static_cast<Button*>(userData);
+    if (self && self->userCallback) {
+        self->userCallback();
+    }
+    return EM_TRUE;
+}
 
-Button::Button(const std::string &id, std::function<void()> callback) : id(id) {
-    onClickCallback = callback;
-    element = val::global("document").call<val>("getElementById", id);
-
-    element.call<void>(
-        "addEventListener", 
-        std::string("click"), 
-        val::module_property("onClick")
+Button::Button(const std::string& buttonId, std::function<void()> callback) 
+    : elementId(buttonId[0] == '#' ? buttonId : "#" + buttonId),
+      userCallback(callback) 
+{
+    emscripten::val element = emscripten::val::global("document").call<emscripten::val>("querySelector", elementId);
+    if (element.isNull()) {
+        std::cerr << "Button element not found: " << elementId << std::endl;
+    }
+    
+    emscripten_set_click_callback(
+        elementId.c_str(),
+        this,
+        EM_FALSE,
+        &Button::handleClick
     );
 }
 
-void Button::onClick() {
-    if (onClickCallback) onClickCallback();
-}
-
-EMSCRIPTEN_BINDINGS(button_bindings) {
-    class_<Button>("Button")
-        .constructor<std::string, std::function<void()>>()
-        .function("onClick", &Button::onClick);
+Button::~Button() {
+    emscripten_set_click_callback(
+        elementId.c_str(),
+        nullptr,
+        EM_FALSE,
+        nullptr
+    );
 }
--- src/drawing-system/Button.h
+++ src/drawing-system/Button.h
@@ -1,20 +1,23 @@
-// Button.h
 #pragma once
-
 #include <string>
+#include <iostream>
 #include <functional>
 #include <emscripten.h>
+#include <emscripten/html5.h>
 #include <emscripten/val.h>
-#include <emscripten/bind.h>
 
 class Button {
-public:
-    Button(const std::string &id, std::function<void()> callback);
-
-    void onClick();
-
 private:
-    std::string id;
     emscripten::val element;
-    std::function<void()> onClickCallback;
+    std::string elementId;
+    std::function<void()> userCallback;
+    
+    static EM_BOOL handleClick(int eventType, const EmscriptenMouseEvent* e, void* userData);
+
+public:
+    Button(const std::string& selector, std::function<void()> callback);
+    ~Button();
+    
+    Button(const Button&) = delete;
+    Button& operator=(const Button&) = delete;
 };
